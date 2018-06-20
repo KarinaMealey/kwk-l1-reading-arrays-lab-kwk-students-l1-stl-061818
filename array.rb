a9f652efa2f7261a75aa4bd209fdab4c626c144c
KarinaMealey <kem32002@gmail.com>
2018-06-20 15:26:25 +0000

    automatically backed up by learn

--- array.rb
+++ array.rb
@@ -1,16 +1,2 @@
 
 
-disney = ["Moana", "Elsa", "Cinderella", "Mickey Mouse", "Mulan"].length
-
-puts disney 
-
-
-age = [16, 16, 14, 15].length
-puts age
-
-
-# book = ["Harry Potter", "To Kill a Mockingbird", "A Tree Grows in Brooklyn", "19 Minutes"]
-
-
-# book.push("geometry textbook")
-# puts book
