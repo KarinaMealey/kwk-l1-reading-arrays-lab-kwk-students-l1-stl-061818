4c4974b30aafb19ee777cab5e92a3c763f6a164d
KarinaMealey <kem32002@gmail.com>
2018-06-20 15:00:51 +0000

    automatically backed up by learn

--- array.rb
+++ array.rb
@@ -4,4 +4,4 @@ puts disney
 age = [16, 16, 14, 15]
 puts age
 
-book = ["Harry Potter", "To Kill a Mockingbird", "A Tree Grows in Brooklyn", ""]
+book = ["Harry Potter", "To Kill a Mockingbird", "A Tree Grows in Brooklyn", "19 Minutes"]
