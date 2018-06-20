43798d1a4d886d18eb142538ee8ae82c422e9ba3
KarinaMealey <kem32002@gmail.com>
2018-06-20 15:16:09 +0000

    automatically backed up by learn

--- array.rb
+++ array.rb
@@ -1,5 +1,5 @@
-# disney = ["Moana", "Elsa", "Cinderella", "Mickey Mouse", "Mulan"]
-# puts disney 
+disney = ["Moana", "Elsa", "Cinderella", "Mickey Mouse", "Mulan"]
+puts disney 
 
 
 # age = [16, 16, 14, 15]
