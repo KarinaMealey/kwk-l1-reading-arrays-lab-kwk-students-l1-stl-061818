731833a62b9de7b6e094650fa410bc68ab1cc726
KarinaMealey <kem32002@gmail.com>
2018-06-20 14:57:43 +0000

    automatically backed up by learn

--- array.rb
+++ array.rb
@@ -1,12 +1,2 @@
-# student_one = "Caroline"
-# student_two = "Millie"
-# student_three = "Sam"
-# student_four = "Sonali"
-
-# 
-
-student_list = ["Caroline", "Millie", "Sam", "Sonali"]
-puts student_list
-
-friends = ["Ross", "Rachel", "Chandler", "Joey", "Monica", "Phoebe"]
-puts friends
+disney = ["Moana", "Elsa", "Cinderella", "Mickey Mouse", "Mulan"]
+puts disney 
