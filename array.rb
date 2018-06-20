203c8406b7446438dcbdec38dba8be6bb2b4af59
KarinaMealey <kem32002@gmail.com>
2018-06-20 15:08:28 +0000

    automatically backed up by learn

--- array.rb
+++ array.rb
@@ -1,8 +1,11 @@
-disney = ["Moana", "Elsa", "Cinderella", "Mickey Mouse", "Mulan"]
-puts disney 
+# disney = ["Moana", "Elsa", "Cinderella", "Mickey Mouse", "Mulan"]
+# puts disney 
 
-age = [16, 16, 14, 15]
-puts age
+
+# age = [16, 16, 14, 15]
+# puts age
+
 
 book = ["Harry Potter", "To Kill a Mockingbird", "A Tree Grows in Brooklyn", "19 Minutes"]
-puts book
+
+puts book[0]
